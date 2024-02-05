f1605f2420e2042b37b7f204ff3e2dfe32e56b0f
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-02-05 10:57:40 -0300

    create app and add colors projetc, images and icon

---
+++ DictionMaster/DictionMaster/DictionMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionMasterApp.swift
+//  DictionMaster
+//
+//  Created by Grazi  Berti on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DictionMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SearchView()
+        }
+    }
+}
---
+++ DictionMaster/DictionMaster/Views/SearchView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DictionMaster
+//
+//  Created by Grazi  Berti on 05/02/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SearchView()
+}
