8087ade6209fb5ea00c882a92b87bb14133d1b04
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-02-05 19:10:29 -0300

    add is bold at multi colors component

--- DictionMaster/DictionMaster/Components/MultiColorText.swift
+++ DictionMaster/DictionMaster/Components/MultiColorText.swift
@@ -14,11 +14,13 @@ struct MultiColorText: View {
         var id: UUID
         var text: String
         var color: Color
+        var isBold: Bool
 
-        init(text: String, color: Color) {
+        init(text: String, color: Color, isBold: Bool = false) {
             self.id = UUID()
             self.text = text
             self.color = color
+            self.isBold = isBold
         }
     }
 
@@ -26,17 +28,17 @@ struct MultiColorText: View {
         components.reduce(Text("")) { result, component in
             result + Text(component.text)
                 .foregroundColor(component.color)
+                .bold(component.isBold)
         }
     }
 }
 
-
-
 #Preview {
     MultiColorText(components: [
-        MultiColorText.TextComponent(text: "Subscribe now to get ", color: Color.theme.textPrimaryColor),
-        MultiColorText.TextComponent(text: " unlimited ", color: Color.theme.buttonColor),
-        MultiColorText.TextComponent(text: " searches full access to", color: Color.theme.textPrimaryColor),
-        MultiColorText.TextComponent(text: " all features.  ", color: Color.theme.buttonColor),
+        MultiColorText.TextComponent(text: "Subscribe now to get ", color: Color.theme.textPrimaryColor, isBold: true),
+        MultiColorText.TextComponent(text: " unlimited ", color: Color.theme.buttonColor, isBold: true),
+        MultiColorText.TextComponent(text: " searches full access to", color: Color.theme.textPrimaryColor, isBold: true),
+        MultiColorText.TextComponent(text: " all features.  ", color: Color.theme.buttonColor, isBold: true),
     ])
+    .font(.title3)
 }
