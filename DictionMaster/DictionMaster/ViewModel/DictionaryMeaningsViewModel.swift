4567e497815e7096abe16e1ea02c7f257011655a
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2024-02-07 13:51:26 -0300

    create view model

---
+++ DictionMaster/DictionMaster/ViewModel/DictionaryMeaningsViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  DictionaryMeaningsViewModel.swift
+//  DictionMaster
+//
+//  Created by Grazi  Berti on 07/02/24.
+//
+
+import Foundation
+
+final class DictionaryMeaningsViewModel: ObservableObject {
+    private let service = APIService()
+    
+    @Published var meanings: [Meanings] = []
+    
+    public func fetchMeanings() {
+        service.getWordMeanings { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let models):
+                    self?.meanings = models
+                case .failure:
+                    break
+                }
+            }
+        }
+    }
+}
